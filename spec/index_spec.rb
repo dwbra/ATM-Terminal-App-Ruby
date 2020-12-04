9b5b3c7e03ba3ca60e2a88ae1a86b3f84e628133
“dwbra” <dworkmang@gmail.com>
2020-12-04 11:42:25 +1100

    initialized bundler. Added rspec

---
+++ spec/index_spec.rb
@@ -0,0 +1,11 @@
+require '../index'
+
+describe 'index.rb' do
+    it 'does stuff' do
+        expect(hello()).to eq("hello world")
+    end
+    
+    xit "does something else" do
+    
+    end
+end
