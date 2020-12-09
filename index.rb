ba96a8ba72972650897123206a585c888a337180
“dwbra” <dworkmang@gmail.com>
2020-12-10 09:37:37 +1100

    added withdrawl and deposit function from fridays challenge

--- index.rb
+++ index.rb
@@ -10,28 +10,56 @@ def welcome_and_input
     puts "3. Make a deposit"
     puts "4. Exit"
     input = gets.chomp.to_i
-    input_valid = Validators.validate_input(input)
-    if !input_valid
-             #print error message for invalid input
-            #re-print options
-            puts "Invalid input, please enter a number from 1-4"
-            puts "Press any key to continue"
-            gets 
-            welcome_and_input()
-        welcome_and_input()
+    # input_valid = Validators.validate_input(input)
+    # if !input_valid
+    #          #print error message for invalid input
+    #         #re-print options
+    #         puts "Invalid input, please enter a number from 1-4"
+    #         puts "Press any key to continue"
+    #         gets 
+    #         welcome_and_input()
+    #     welcome_and_input()
     case input 
         when 1 
             #show balance
+            bank_balance = 0
+            File.open("balance.txt").each do |file|
+                bank_balance = file.to_i
+                puts "Your bank balance is #{bank_balance}."
+            end
         when 2
             #make a withdrawl
+            puts "How much would you like to withdraw?"
+            withdraw = gets.chomp.to_i
+            bank_balance = 0
+            File.open("balance.txt").each do |file|
+                bank_balance = file.to_i
+            end
+            amount_after_withdrawl = bank_balance - withdraw
+            File.open("balance.txt", "w") do |f|
+                f.write amount_after_withdrawl
+            puts "Your bank balance is #{amount_after_withdrawl}."
+            end
         when 3
             #make a deposit
+            puts "How much would you like to deposit?"
+            deposit = gets.chomp.to_i
+            bank_balance = 0
+            File.open("balance.txt").each do |file|
+                bank_balance = file.to_i
+            end
+            amount_after_deposit = bank_balance + deposit
+            File.open("balance.txt", "w") do |f|
+                f.write amount_after_deposit
+            puts "Your bank balance is #{amount_after_deposit}."
+            end
         when 4
             #exit
             puts "Goodbye fucker"
             system("exit")
         else
-    end
-    end
-
+            gets.chomp.to_i
+            welcome_and_input()
+        end
+end
 welcome_and_input()
