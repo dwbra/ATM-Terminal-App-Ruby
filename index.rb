d5253e6fba4620b555dc4b6719326c1cd9bb1e21
“dwbra” <dworkmang@gmail.com>
2020-12-10 11:44:15 +1100

    added the withdrawl function with validators on each step

--- index.rb
+++ index.rb
@@ -1,65 +1,95 @@
-## This method won't be used.
-## Only exists to check that rspect is working correctly 
+ require_relative ("validators")
+ require_relative ("atm_view")
 
-def welcome_and_input
-    system("clear")
-    puts "Welcome to bank ATM"
-    puts "Please select from the following options:"
-    puts "1. Display balance"
-    puts "2. Make a withdrawl"
-    puts "3. Make a deposit"
-    puts "4. Exit"
+system("clear")
+
+def welcome_and_input(atm_view)
+    atm_view.show_options
+
     input = gets.chomp.to_i
-    # input_valid = Validators.validate_input(input)
-    # if !input_valid
-    #          #print error message for invalid input
+    input_valid = Validators.validate_input(input)
+    if !input_valid
+    #         #print error message for invalid input
     #         #re-print options
-    #         puts "Invalid input, please enter a number from 1-4"
-    #         puts "Press any key to continue"
-    #         gets 
-    #         welcome_and_input()
-    #     welcome_and_input()
+            puts "Invalid input, please enter a number from 1-4"
+    end
     case input 
         when 1 
             #show balance
-            bank_balance = 0
-            File.open("balance.txt").each do |file|
-                bank_balance = file.to_i
-                puts "Your bank balance is #{bank_balance}."
-            end
+            show_balance()
         when 2
             #make a withdrawl
-            puts "How much would you like to withdraw?"
-            withdraw = gets.chomp.to_i
-            bank_balance = 0
-            File.open("balance.txt").each do |file|
-                bank_balance = file.to_i
-            end
-            amount_after_withdrawl = bank_balance - withdraw
-            File.open("balance.txt", "w") do |f|
-                f.write amount_after_withdrawl
-            puts "Your bank balance is #{amount_after_withdrawl}."
-            end
+            make_withdrawl()
         when 3
             #make a deposit
-            puts "How much would you like to deposit?"
-            deposit = gets.chomp.to_i
-            bank_balance = 0
-            File.open("balance.txt").each do |file|
-                bank_balance = file.to_i
-            end
-            amount_after_deposit = bank_balance + deposit
-            File.open("balance.txt", "w") do |f|
-                f.write amount_after_deposit
-            puts "Your bank balance is #{amount_after_deposit}."
-            end
+            make_deposit()
         when 4
             #exit
             puts "Goodbye fucker"
-            system("exit")
-        else
-            gets.chomp.to_i
-            welcome_and_input()
+            exit(0)
         end
+    welcome_and_input(atm_view)
 end
-welcome_and_input()
+
+#show balance method
+def show_balance
+    balance = File.read("balance.txt")
+    puts "balance: $#{balance}"
+end
+
+def get_balance
+    File.read("balance.txt")
+end
+
+#withdrawl method
+def make_withdrawl
+    puts "How much would you like to withdraw?"
+    #get the amount
+    amount = gets.chomp
+    #validate the input
+    valid = Validators.validate_withdrawl(amount)
+    if !valid
+        #print out error mesage
+        puts "invalid amount, please enter a positive number"
+        make_withdrawl()
+    end
+    balance = get_balance()
+    valid = Validators.validate_amount_against_balance(amount, balance)
+    if !valid
+        #make the withdrawl
+        #get the bank balance
+        #
+        puts "Your withdrawl is greater than your balance"
+        make_withdrawl()
+    end
+    updated_amount = (balance.to_i - amount.to_i).to_s
+    File.write("balance.txt", updated_amount)
+    puts "Your new balance is $#{updated_amount}"
+    # withdraw = gets.chomp.to_i
+    # bank_balance = 0
+    # File.open("balance.txt").each do |file|
+    #     bank_balance = file.to_i
+    # end
+    # amount_after_withdrawl = bank_balance - withdraw
+    # File.open("balance.txt", "w") do |f|
+    #     f.write amount_after_withdrawl
+    # puts "Your bank balance is #{amount_after_withdrawl}."
+end
+
+#deposit method
+def make_deposit
+    puts "How much would you like to deposit?"
+    deposit = gets.chomp.to_i
+    bank_balance = 0
+    File.open("balance.txt").each do |file|
+        bank_balance = file.to_i
+    end
+    amount_after_deposit = bank_balance + deposit
+    File.open("balance.txt", "w") do |f|
+        f.write amount_after_deposit
+    puts "Your bank balance is #{amount_after_deposit}."
+    end
+end
+
+atm_view = AtmView.new
+welcome_and_input(atm_view)
